120b15b87e1968565be7a7536a41bf6a55b626f7
Jeral <jeral.cris@gmail.com>
2021-06-02 13:11:16 -0600

    Formulario de registro de usuarios

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useFrom';
 
 export const RegisterScreen = () => {
+   const [formValues, handleInputChange] = useForm({
+      name: 'Jeral',
+      email: 'jeral@gmail.com',
+      password: '12345',
+      password2: '12345',
+   });
+
+   const { name, email, password, password2 } = formValues;
+
+   const handleRegister = (e) => {
+      e.preventDefault();
+
+      console.log(name, email, password, password2);
+   };
+
+   const isFormValid = () => {};
+
    return (
       <>
          <h3 className='auth__title'>Register</h3>
 
-         <form action=''>
-            <input type='text' placeholder='Name' name='name' className='auth__input' autoComplete='off' />
+         <form onSubmit={handleRegister}>
+            <input type='text' placeholder='Name' name='name' className='auth__input' autoComplete='off' value={name} onChange={handleInputChange} />
 
-            <input type='text' placeholder='Email' name='email' className='auth__input' autoComplete='off' />
+            <input type='text' placeholder='Email' name='email' className='auth__input' autoComplete='off' value={email} onChange={handleInputChange} />
 
-            <input type='password' placeholder='Password' name='password' className='auth__input' />
+            <input type='password' placeholder='Password' name='password' className='auth__input' value={password} onChange={handleInputChange} />
 
-            <input type='password' placeholder='Confirm password' name='password2' className='auth__input' />
+            <input type='password' placeholder='Confirm password' name='password2' className='auth__input' value={password2} onChange={handleInputChange} />
 
             <button className='btn btn-primary btn-block mb-5' type='submit'>
                Register
