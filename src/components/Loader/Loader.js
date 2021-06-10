833797b23d246f853627565ba81a7aaed0b11ec7
Jeral <jeral.cris@gmail.com>
2021-06-10 11:28:32 -0600

    Mostrar un loading global en la app

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Loader = () => {
+   return (
+      <div class='loader-background'>
+         <div class='loader'></div>
+      </div>
+   );
+};
--- src/components/routers/AppRouter.js
+++ src/components/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
@@ -7,17 +7,31 @@ import { firebase } from '../../firebase/firebase-config';
 import { AuthRouter } from './AuthRouter';
 import { JournalScreen } from '../journal/JournalScreen';
 import { login } from '../../actions/auth';
+import { Loader } from '../Loader/Loader';
 
 export const AppRouter = () => {
    const dispatch = useDispatch();
+
+   const [checking, setChecking] = useState(true);
+   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
    useEffect(() => {
       firebase.auth().onAuthStateChanged((user) => {
          if (user?.uid) {
             dispatch(login(user.uid, user.displayName));
+            setIsLoggedIn(true);
+         } else {
+            setIsLoggedIn(false);
          }
+
+         //  Ya tengo la respuesta de la auth
+         setChecking(false);
       });
-   }, []);
+   }, [dispatch, setChecking, setIsLoggedIn]);
+
+   if (checking) {
+      return <Loader />;
+   }
 
    return (
       <Router>
