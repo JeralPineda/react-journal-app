4cfe766f8287ea3b4a162f5994f90f7d5bbb0358
Jeral <jeral.cris@gmail.com>
2021-06-10 12:18:33 -0600

    SweetAlert2 mensajes de error

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,6 @@
-import { types } from '../types/types';
+import Swal from 'sweetalert2';
 
+import { types } from '../types/types';
 import { firebase, googleAuthProvider } from '../firebase/firebase-config';
 import { finishLoading, startLoading } from './ui';
 
@@ -18,6 +19,7 @@ export const startLoginEmailPassword = (email, password) => {
          .catch((e) => {
             console.log(e);
             dispatch(finishLoading());
+            Swal.fire('Error', e.message, 'error');
          });
    };
 };
@@ -33,6 +35,7 @@ export const startRegisterWithEmailPasswordName = (email, password, name) => {
          })
          .catch((e) => {
             console.log(e);
+            Swal.fire('Error', e.message, 'error');
          });
    };
 };
