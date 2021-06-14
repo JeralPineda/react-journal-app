7fdb4ca8fbcf8796241c409eb97dbdcdf2dbf01f
Jeral <jeral.cris@gmail.com>
2021-06-14 14:42:54 -0600

    notesReducer

---
+++ src/reducers/notesReducers.js
@@ -0,0 +1,25 @@
+/*
+    {
+        notes: [],
+        active: null,
+        active: {
+            id = 'envnejvore gonv6151b5',
+            title: '' ,
+            body: '',
+            imageUrl: '',
+            date: 1226516115
+        }
+    }
+*/
+
+const initialState = {
+   notes: [],
+   active: null,
+};
+
+export const notesReducer = (state = initialState, action) => {
+   switch (action.type) {
+      default:
+         return state;
+   }
+};
