a8eadef6abcb1d5df6a09f4191c3b3e31d74df19
Jeral <jeral.cris@gmail.com>
2021-06-14 21:07:41 -0600

    Mostrando la info en pantalla de la nota a editar

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useForm } from '../../hooks/useFrom';
 import { NotesAppBar } from './NotesAppBar';
 
 export const NoteScreen = () => {
+   const { active: note } = useSelector((state) => state.notes);
+   const [formValues, handleInputChange] = useForm(note);
+
+   console.log(formValues);
+   const { body, title } = formValues;
+
    return (
       <div className='notes__main-content'>
          <NotesAppBar />
 
          <div className='notes__content'>
-            <input type='text' placeholder='Some awesome title' className='notes__title-input' autoComplete='off' />
+            <input type='text' placeholder='Some awesome title' className='notes__title-input' autoComplete='off' value={title} onChange={handleInputChange} />
 
-            <textarea placeholder='What happened today' className='notes__textarea'></textarea>
-
-            <div className='notes__image'>
-               <img src='https://www.cice.es/wp-content/uploads/2015/03/pez-artwork-ilustraciones.jpg' alt='imagen' />
-            </div>
+            <textarea placeholder='What happened today' className='notes__textarea' value={body} onChange={handleInputChange}></textarea>
+            {note.url && (
+               <div className='notes__image'>
+                  <img src='https://www.cice.es/wp-content/uploads/2015/03/pez-artwork-ilustraciones.jpg' alt='imagen' />
+               </div>
+            )}
          </div>
       </div>
    );
