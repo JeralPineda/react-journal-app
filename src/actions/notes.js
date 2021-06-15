37e7d8712a6e0a93713a3ff09cbfa274bf4d5a05
Jeral <jeral.cris@gmail.com>
2021-06-14 22:16:33 -0600

    Actualizar la nota actual

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -3,8 +3,8 @@ import { loadNotes } from '../helpers/loadNotes';
 import { types } from '../types/types';
 
 export const startNewNote = () => {
-   return async (dispatch, getSatte) => {
-      const { uid } = getSatte().auth;
+   return async (dispatch, getState) => {
+      const { uid } = getState().auth;
 
       const newNote = {
          title: '',
@@ -38,3 +38,18 @@ export const setNotes = (notes) => ({
    type: types.notesLoad,
    payload: notes,
 });
+
+export const startSaveNote = (note) => {
+   return async (dispatch, getState) => {
+      const { uid } = getState().auth;
+
+      if (!note.url) {
+         delete note.url;
+      }
+
+      const noteToFirestore = { ...note };
+      delete noteToFirestore.id;
+
+      await db.doc(`${uid}/journal/notes/${note.id}`).update(noteToFirestore);
+   };
+};
--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { useEffect, useRef } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { activeNote } from '../../actions/notes';
 import { useForm } from '../../hooks/useFrom';
 import { NotesAppBar } from './NotesAppBar';
 
 export const NoteScreen = () => {
+   const dispatch = useDispatch();
+
    const { active: note } = useSelector((state) => state.notes);
    const [formValues, handleInputChange, reset] = useForm(note);
    const { body, title } = formValues;
@@ -18,14 +21,19 @@ export const NoteScreen = () => {
       }
    }, [note, reset]);
 
+   useEffect(() => {
+      dispatch(activeNote(formValues.id, { ...formValues }));
+   }, [formValues, dispatch]);
+
    return (
       <div className='notes__main-content'>
          <NotesAppBar />
 
          <div className='notes__content'>
-            <input type='text' placeholder='Some awesome title' className='notes__title-input' autoComplete='off' value={title} onChange={handleInputChange} />
+            <input type='text' placeholder='Some awesome title' className='notes__title-input' autoComplete='off' value={title} onChange={handleInputChange} name='title' />
 
-            <textarea placeholder='What happened today' className='notes__textarea' value={body} onChange={handleInputChange}></textarea>
+            <textarea placeholder='What happened today' className='notes__textarea' value={body} onChange={handleInputChange} name='body'></textarea>
+
             {note.url && (
                <div className='notes__image'>
                   <img src='https://www.cice.es/wp-content/uploads/2015/03/pez-artwork-ilustraciones.jpg' alt='imagen' />
