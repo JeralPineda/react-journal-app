f2ee1050f85fa13434d915626fb488e4ab8da2b6
Jeral <jeral.cris@gmail.com>
2021-06-14 20:34:36 -0600

    Mostrando las notas en el menu lateral

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import moment from 'moment';
 
-export const JournalEntry = () => {
+export const JournalEntry = ({ id, date, title, body, url }) => {
+   const noteDate = moment(date);
+
    return (
       <div className='journal__entry pointer'>
-         <div
-            className='journal__entry-picture'
-            style={{
-               backgroundSize: 'cover',
-               backgroundImage: 'url(https://www.cice.es/wp-content/uploads/2015/03/pez-artwork-ilustraciones.jpg)',
-            }}
-         ></div>
-
+         {url && (
+            <div
+               className='journal__entry-picture'
+               style={{
+                  backgroundSize: 'cover',
+                  backgroundImage: `url(${url})`,
+               }}
+            ></div>
+         )}
          <div className='journal__entry-body'>
-            <p className='journal__entry-title'>Un nuevo dia</p>
+            <p className='journal__entry-title'>{title}</p>
 
-            <p className='journal__entry-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ut quia dolorum! </p>
+            <p className='journal__entry-content'>{body}</p>
          </div>
-
          <div className='journal__entry-date-box'>
-            <span>Monday</span>
-            <h4>30</h4>
+            <span>{noteDate.format('dddd')}</span>
+            <h4>{noteDate.format('Do')}</h4>
          </div>
       </div>
    );
