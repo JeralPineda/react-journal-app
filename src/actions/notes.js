cb46e37f16e13efb06e1fdf6699b52263b1c708b
Jeral <jeral.cris@gmail.com>
2021-06-14 23:42:05 -0600

    Subir imagen a cloudDinary

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,6 +1,7 @@
 import Swal from 'sweetalert2';
 
 import { db } from '../firebase/firebase-config';
+import { fileUpload } from '../helpers/fileUpload';
 import { loadNotes } from '../helpers/loadNotes';
 import { types } from '../types/types';
 
@@ -69,3 +70,15 @@ export const refreshNote = (id, note) => ({
       },
    },
 });
+
+export const startUploading = (file) => {
+   return async (dispatch, getState) => {
+      const { active: activeNote } = getState().notes;
+
+      //   Cargar las imágenes
+      const fileUrl = await fileUpload(file);
+
+      console.log(fileUrl);
+      console.log(activeNote);
+   };
+};
---
+++ src/helpers/fileUpload.js
@@ -0,0 +1,23 @@
+export const fileUpload = async (file) => {
+   const cloudUrl = 'https://api.cloudinary.com/v1_1/dyszc49bz/upload';
+
+   const formData = new FormData();
+   formData.append('upload_preset', 'react-journal');
+   formData.append('file', file);
+
+   try {
+      const resp = await fetch(cloudUrl, {
+         method: 'POST',
+         body: formData,
+      });
+
+      if (resp.ok) {
+         const cloundResp = await resp.json();
+         return cloundResp.secure_url;
+      } else {
+         throw await resp.json();
+      }
+   } catch (error) {
+      throw error;
+   }
+};
