d5ead7ade700159d6de8a43d0a78aaad7e797561
Jeral <jeral.cris@gmail.com>
2021-06-16 23:37:32 -0600

    configurando  base de datos para testing

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -11,8 +11,24 @@ const firebaseConfig = {
    appId: '1:60412900969:web:b6e77692ceb97dabe214f8',
 };
 
+const firebaseConfigTesting = {
+   apiKey: 'AIzaSyDdcMpYr6xsRAtMubqot6z5oCvsvPBxJ-w',
+   authDomain: 'redux-demo-database.firebaseapp.com',
+   projectId: 'redux-demo-database',
+   storageBucket: 'redux-demo-database.appspot.com',
+   messagingSenderId: '556018416573',
+   appId: '1:556018416573:web:39d43745eefffcf69870d4',
+};
+
+if (process.env.NODE_ENV === 'test') {
+   // testing
+   firebase.initializeApp(firebaseConfigTesting);
+} else {
+   //dev/prod
+   firebase.initializeApp(firebaseConfig);
+}
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
