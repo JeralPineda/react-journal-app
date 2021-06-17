2c8f478d9e9c2ebc020dceafddc04f98a58fbf0f
Jeral <jeral.cris@gmail.com>
2021-06-17 00:33:52 -0600

    Incluyendo variables de entorno

--- README.md
+++ README.md
@@ -5,3 +5,19 @@ Aplicación con React, Firebase, Redux y Sass
 ```
 npm install
 ```
+
+-  Crear 2 archivos de variables de entorno para producción y pruebas:
+
+   ```
+   .env.test
+   .env.development
+   ```
+
+   ```
+   REACT_APP_APIKEY=
+   REACT_APP_AUTHDOMAIN=
+   REACT_APP_PROJECTID=
+   REACT_APP_STORAGEBUCKET=
+   REACT_APP_MESSAGINGSENDERID=
+   REACT_APP_APPID=
+   ```
--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -3,32 +3,41 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-   apiKey: 'AIzaSyDDChMmCbfc_w6f9dg2QbIS4Y_FdoaRl3Y',
-   authDomain: 'react-app-cursos-ffa28.firebaseapp.com',
-   projectId: 'react-app-cursos-ffa28',
-   storageBucket: 'react-app-cursos-ffa28.appspot.com',
-   messagingSenderId: '60412900969',
-   appId: '1:60412900969:web:b6e77692ceb97dabe214f8',
+   apiKey: process.env.REACT_APP_APIKEY,
+   authDomain: process.env.REACT_APP_AUTHDOMAIN,
+   projectId: process.env.REACT_APP_PROJECTID,
+   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+   appId: process.env.REACT_APP_APPID,
 };
 
-const firebaseConfigTesting = {
-   apiKey: 'AIzaSyDdcMpYr6xsRAtMubqot6z5oCvsvPBxJ-w',
-   authDomain: 'redux-demo-database.firebaseapp.com',
-   projectId: 'redux-demo-database',
-   storageBucket: 'redux-demo-database.appspot.com',
-   messagingSenderId: '556018416573',
-   appId: '1:556018416573:web:39d43745eefffcf69870d4',
-};
+// const firebaseConfig = {
+//    apiKey: 'AIzaSyDDChMmCbfc_w6f9dg2QbIS4Y_FdoaRl3Y',
+//    authDomain: 'react-app-cursos-ffa28.firebaseapp.com',
+//    projectId: 'react-app-cursos-ffa28',
+//    storageBucket: 'react-app-cursos-ffa28.appspot.com',
+//    messagingSenderId: '60412900969',
+//    appId: '1:60412900969:web:b6e77692ceb97dabe214f8',
+// };
 
-if (process.env.NODE_ENV === 'test') {
-   // testing
-   firebase.initializeApp(firebaseConfigTesting);
-} else {
-   //dev/prod
-   firebase.initializeApp(firebaseConfig);
-}
+// const firebaseConfigTesting = {
+//    apiKey: 'AIzaSyDdcMpYr6xsRAtMubqot6z5oCvsvPBxJ-w',
+//    authDomain: 'redux-demo-database.firebaseapp.com',
+//    projectId: 'redux-demo-database',
+//    storageBucket: 'redux-demo-database.appspot.com',
+//    messagingSenderId: '556018416573',
+//    appId: '1:556018416573:web:39d43745eefffcf69870d4',
+// };
 
-// Initialize Firebase
+// if (process.env.NODE_ENV === 'test') {
+//    // testing
+//    firebase.initializeApp(firebaseConfigTesting);
+// } else {
+//    //dev/prod
+//    firebase.initializeApp(firebaseConfig);
+// }
+
+firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
